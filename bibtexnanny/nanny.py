a4589a9ba8bb4d39d950d32e3272da3b139aa926
Marc Schulder <961375+marcschulder@users.noreply.github.com>
2018-06-15 11:59:45 +0200

    Implemented fixer function for unsecured uppercase chars in title

--- bibtexnanny/nanny.py
+++ bibtexnanny/nanny.py
@@ -4,15 +4,18 @@
 Collection of functions used by various tools of the BibTexNanny toolkit.
 """
 
-__author__ = 'Marc Schulder'
-
 import sys
 import os
 import re
+from collections import OrderedDict
+
 import biblib.bib
+
+__author__ = 'Marc Schulder'
 
 
 def loadBibTex(filename):
+    # TODO: Load preamble
     if not (os.path.exists(filename) and os.path.isfile(filename)):
         raise FileNotFoundError(filename)
 
@@ -28,6 +31,29 @@ def loadBibTex(filename):
     return entries
 
 
+def loadCitedKeys(filename):
+    return []
+
+
+def filterEntries(key2entry, keyWhitelist):
+    keyWhitelist = set(keyWhitelist)
+    filteredEntries = OrderedDict()
+
+    for key, entry in key2entry.items():
+        if key in keyWhitelist:
+            filteredEntries[key] = entry
+
+    return filteredEntries
+
+
+def saveBibTex(filename, key2entry):
+    # TODO: save preamble
+    entryStrings = [entry.to_bib(wrap_width=None) for entry in key2entry.values()]
+    text = '\n\n'.join(entryStrings)
+    with open(filename, 'w') as w:
+        w.write(text)
+
+
 def findDuplicateKeys(entries):
     # Can not be checked for right now because biblib throws errors when encountering repeated keys
 
@@ -40,11 +66,11 @@ def findDuplicateKeys(entries):
     # return duplicates
 
 
-def findDuplicateTitles(entries, ignoreCurlyBrackets=True, ignoreCaps=True):
+def findDuplicateTitles(entries, ignoreCurlyBraces=True, ignoreCaps=True):
     seen = {}
     for key, entry in entries.items():
         title = entry["title"]
-        if ignoreCurlyBrackets:
+        if ignoreCurlyBraces:
             title = title.replace('{', '')
             title = title.replace('}', '')
         if ignoreCaps:
@@ -62,31 +88,34 @@ def findDuplicateTitles(entries, ignoreCurlyBrackets=True, ignoreCaps=True):
 
 def findUnsecuredUppercase(entries):
     """
-    Find entries that contain uppercase characters that are not secured by {}-brackets.
-    The only uppercase character that needs no brackets is the first letter of the title, as it is automatically
+    Find entries that contain uppercase characters that are not secured by curly braces.
+    The only uppercase character that needs no braces is the first letter of the title, as it is automatically
     converted to uppercase anyway.
 
     Background: In most bibliography styles titles are by defauly displayed with only the first character being
     uppercase and all others are forced to be lowercase. If you want to display any other uppercase characters, you
-    have to secure them by wrapping them in {}-brackets.
+    have to secure them by wrapping them in curly braces.
     :param entries:
     :return:
     """
-    unsecured = []
+    key2unsecuredChars = {}
     for key, entry in entries.items():
         title = entry["title"]
         isSecured = False
 
-        # Skip first character (unless it is a curly bracket) as it is auto-converted to uppercase anyway
+        # Skip first character (unless it is a curly brace) as it is auto-converted to uppercase anyway
+        # full_title = title
+        i_start = 0
         if title[0] != '{':
             title = title[1:]
+            i_start = 1
 
-        for c in title:
+        for i, c in enumerate(title, start=i_start):
             if c == '{':
                 isSecured = True
             elif c == '}':
                 isSecured = False
             elif not isSecured and c.isupper():
-                unsecured.append(key)
-                break
-    return unsecured
+                # print("Unsecured character {} ({}): {}".format(c, i, full_title))
+                key2unsecuredChars.setdefault(key, []).append(i)
+    return key2unsecuredChars
